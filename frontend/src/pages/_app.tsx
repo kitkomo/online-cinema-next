82564a42286276eb57fa11481ba2616d82ebfb28
quive <unjusodal@gmail.com>
2023-05-15 18:30:41 +0300

    redux toolkit + toastr

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import type { AppProps } from 'next/app'
 import MainProvider from 'providers/MainProvider'
 
-import Layout from '@/components/layout/Layout'
-
 import '@/assets/scss/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<MainProvider>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<Component {...pageProps} />
 		</MainProvider>
 	)
 }
